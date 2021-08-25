5b16f6807e9c600af96dea46e1914126ca5f09b9
tomas votroubek <tomas.votroubek@outlook.com>
2021-08-25 03:14:07 +0200

    add logging

--- src/branch_and_bound.jl
+++ src/branch_and_bound.jl
@@ -1,6 +1,8 @@
 
 using DataStructures: BinaryMinHeap
+using ProgressMeter
 
+include("logger.jl")
 include("pop_while.jl")
 include("locking_atomic.jl")
 include("concurrent_heap.jl")
@@ -12,19 +14,23 @@ function solve_root(node, bound, branch)
 	LockingAtomic(feasible), ConcurrentHeap(BinaryMinHeap(branches))
 end
 
-function branch_and_bound(root, bound, branch; gap=eps())
+function branch_and_bound(root, bound, branch; gap=eps(), out=stderr)
 	incumbent, work = solve_root(root, bound, branch)
 	not_fathom(x) = !isapprox(first(incumbent[]), first(x); atol=gap)
 	iter = PopWhile(not_fathom, work)
+	log = logger(out, gap)
 
-	Threads.foreach(iter) do (_, node)
+	Threads.foreach(iter) do (prior, node)
 		lower, feasible... = bound(node)
 		branches = tuple.(lower, branch(node))
 
 		atomic_min!(incumbent, feasible)
 		pruned = filter(not_fathom, branches)
 		push!(work, pruned...)
+
+		update(log, first(incumbent[]), prior, lower, feasible...)
 	end
 
+	finish(log)
 	incumbent[]
 end
---
+++ src/logger.jl
@@ -0,0 +1,26 @@
+struct Logger{O}
+	tgt::O
+	gap
+end
+
+function logger(io::Base.TTY, gap)
+	progress = ProgressThresh(gap; desc="B&B:", output=io, showspeed=true)
+	Logger(progress, gap)
+end
+
+function logger(io::IOStream, gap)
+	Logger(io, gap)
+end
+
+function update(l::Logger{<:ProgressThresh}, inc, prior, lower, feas, cut)
+	vals = [(:lower, lower), (:feasible, feas)]
+	ProgressMeter.update!(l.tgt, inc - prior, showvalues=vals)
+end
+
+function update(l::Logger{<:IOStream}, inc, prior, lower, feas, cut)
+	println(l.tgt, join([lower, feas], " "))
+end
+
+finish(l::Logger{ProgressThresh}) = ProgressMeter.finish!(l.tgt)
+
+finish(l::Logger{IOStream}) = nothing
